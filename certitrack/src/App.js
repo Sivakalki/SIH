49cf05d683337faada1dced1fcd733916801b8b0
sivakalki <sivakalkipusarla6@gmail.com>
2024-11-13 15:31:07 +0530

    added tailwind component

--- certitrack/src/App.js
+++ certitrack/src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div>
-      Welcome to react app 
+      CertiTrack Application
+      <div className="buttons flex flex-col">
+        <button>Signup</button>
+        <button>Log in</button>
+      </div>
     </div>
   );
 }
